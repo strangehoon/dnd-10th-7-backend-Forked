9399dcfba17328969bf5e9fab312d7e72325201b
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2024-03-30 19:16:56 +0900

    fix : 빈 참조 에러 해결 #45

--- src/main/java/com/sendback/global/config/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/sendback/global/config/auth/JwtAuthenticationFilter.java
@@ -14,7 +14,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
 
